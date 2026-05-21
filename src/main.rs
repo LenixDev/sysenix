c1ca8edbe925ea1456aa1aeedfe5ef003d7ee832
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-05-21 23:35:14 +0100

    refactor(sbi): extract tray icon setup into separate sbi module

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,5 @@
-use tray_icon::{TrayIconBuilder, menu::{Menu, MenuItem, MenuEvent}};
-use winit::application::ApplicationHandler;
-use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, EventLoop};
-use winit::window::WindowId;
-
-struct App {
-  _tray: tray_icon::TrayIcon,
-}
-
-impl ApplicationHandler for App {
-  fn resumed(&mut self, _event_loop: &ActiveEventLoop) {}
-
-  fn window_event(&mut self, _event_loop: &ActiveEventLoop, _id: WindowId, _event: WindowEvent) {}
-
-  fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
-    if let Ok(event) = MenuEvent::receiver().try_recv() {
-      println!("menu clicked: {:?}", event);
-      event_loop.exit();
-    }
-  }
-}
-
-fn load_icon(path: &str) -> tray_icon::Icon {
-  let file = std::fs::File::open(path).unwrap();
-  let decoder = png::Decoder::new(file);
-  let mut reader = decoder.read_info().unwrap();
-  let mut buf = vec![0; reader.output_buffer_size()];
-  let info = reader.next_frame(&mut buf).unwrap();
-  let bytes = &buf[..info.buffer_size()];
-  tray_icon::Icon::from_rgba(bytes.to_vec(), info.width, info.height).unwrap()
-}
+mod sbi;
 
 fn main() {
-  let event_loop = EventLoop::new().unwrap();
-
-  let menu = Menu::new();
-  let quit = MenuItem::new("Quit", true, None);
-  menu.append(&quit).unwrap();
-
-	let icon = load_icon("assets/favicon-light.png");
-
-  let tray = TrayIconBuilder::new()
-		.with_icon(icon)
-    .with_menu(Box::new(menu))
-    .build()
-    .unwrap();
-
-  let mut app = App { _tray: tray };
-  event_loop.run_app(&mut app).unwrap();
+  sbi::main();
 }
