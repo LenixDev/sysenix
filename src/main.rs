7f32aeb118f15ebdf0c2e8f7029bccce51d90f3f
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-05-21 13:54:17 +0100

    chore(cargo): add new dependencies and update Cargo.lock and Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use tray_icon::{TrayIconBuilder, menu::{Menu, MenuItem, MenuEvent}};
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::window::WindowId;
+
+struct App {
+  _tray: tray_icon::TrayIcon,
+}
+
+impl ApplicationHandler for App {
+  fn resumed(&mut self, _event_loop: &ActiveEventLoop) {}
+
+  fn window_event(&mut self, _event_loop: &ActiveEventLoop, _id: WindowId, _event: WindowEvent) {}
+
+  fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+    if let Ok(event) = MenuEvent::receiver().try_recv() {
+      println!("menu clicked: {:?}", event);
+      event_loop.exit();
+    }
+  }
+}
+
 fn main() {
-	
+  let event_loop = EventLoop::new().unwrap();
+
+  let menu = Menu::new();
+  let quit = MenuItem::new("Quit", true, None);
+  menu.append(&quit).unwrap();
+
+  let tray = TrayIconBuilder::new()
+    .with_title("🤖")
+    .with_menu(Box::new(menu))
+    .build()
+    .unwrap();
+
+  let mut app = App { _tray: tray };
+  event_loop.run_app(&mut app).unwrap();
 }
