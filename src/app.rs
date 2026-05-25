dede2bf920dc491ff8ea9448683ab1aba2f04f1b
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-05-25 20:59:31 +0100

    feat(app): load appropriate favicon for light/dark mode and add dark icon asset

--- src/app.rs
+++ src/app.rs
@@ -75,8 +75,14 @@ fn sbi_handle_click(delegate: *mut Object) {
 
 /* COMPONENTS */
 fn sbi_set_app_icon() {
-  unsafe {
-    let icon_path = NSString::alloc(nil).init_str("assets/favicon-light.png");
+	unsafe {
+		let appearance: id = msg_send![NSApp(), effectiveAppearance];
+		let name: id = msg_send![appearance, name];
+		let cstr: *const i8 = msg_send![name, UTF8String];
+		let ns_mode = std::ffi::CStr::from_ptr(cstr).to_str().unwrap();
+		let theme = if ns_mode == "NSAppearanceNameAqua" { "light" } else { "dark" };
+		let path = format!("assets/favicon-{}.png", theme);
+		let icon_path = NSString::alloc(nil).init_str(&path);
     let icon_image: id = msg_send![class!(NSImage), alloc]; /* just an allocated object */
     let icon_image: id = msg_send![icon_image, initWithContentsOfFile: icon_path];
     let _: () = msg_send![STATUS_ITEM.button(), setImage: icon_image];
