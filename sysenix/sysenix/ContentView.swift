348f7eda9aafd135625c2e9ce11dab067456457c
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-06-13 18:59:29 +0100

    chore(project): initial Xcode project scaffold and assets

---
+++ sysenix/sysenix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sysenix
+//
+//  Created by Lenix on 13/6/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sysenix/sysenix/sysenixApp.swift
@@ -0,0 +1,17 @@
+//
+//  sysenixApp.swift
+//  sysenix
+//
+//  Created by Lenix on 13/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct sysenixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
